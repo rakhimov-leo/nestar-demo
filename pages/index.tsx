8809c0b97aca81519d5c6f7e1b335996bdf453f1
Rakhimov Navruz <alina@Rakhimovs-MacBook-Pro.local>
2025-10-15 23:26:24 +0900

    feat: publish layout component

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,18 +4,15 @@ import { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <Stack>
-      <Stack flexDirection={"column"}>
-        <Stack>
-          <Stack className="container">Popular Properties</Stack>
-        </Stack>
-        <Stack>
-          <Stack className="container">Top Agents</Stack>
-        </Stack>
-        <Stack>
-          <Stack className="container">Top Properties</Stack>
-        </Stack>
-        <Box>Events</Box>
+    <Stack className={"home-page"}>
+      <Stack>
+        <Stack className="container">Popular Properties</Stack>
+      </Stack>
+      <Stack>
+        <Stack className="container">Top Agents</Stack>
+      </Stack>
+      <Stack>
+        <Stack className="container">Top Properties</Stack>
       </Stack>
     </Stack>
   );
